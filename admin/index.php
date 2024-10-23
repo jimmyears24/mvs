c7493b93959192584204e342274f72e4946d5b78
jimmyears24 <jimmyears24@proton.me>
2024-10-23 08:29:02 +0200

    Create index.php

---
+++ admin/index.php
@@ -0,0 +1,13 @@
+<?php include 'head.php'; ?>
+
+<?= $alert ?>
+
+<div class="table-responsive p-2">
+
+	<?= $subscriptions_html ?>
+
+</div>
+
+<?= $profiles_html ?>
+
+<?php include 'foot.php'; ?>
